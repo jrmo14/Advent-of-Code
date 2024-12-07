2be918c5a12c6982ba2a01cf30ccbc369f648839
Jackson Moffet <jrmoffet@gmail.com>
2024-12-07 14:04:28 -0500

    Make main run everything again

--- aoc24/bin/main.ml
+++ aoc24/bin/main.ml
@@ -1,9 +1,9 @@
 let () =
-  (* Day1.run; *)
-  (* Day2.run; *)
-  (* Day3.run; *)
-  (* Day4.run; *)
-  (* Day5.run; *)
-  (* This guy is run on test data*)
-  (* Day6.run; *)
+  Day1.run;
+  Day2.run;
+  Day3.run;
+  Day4.run;
+  Day5.run;
+  (* This guy is run on test data *)
+  Day6.run;
   Day7.run
