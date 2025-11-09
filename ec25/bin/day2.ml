4cb94a6d864cb5a3b3d48b4e657cb918d8c67e0c
Jackson Moffet <jrmoffet@gmail.com>
2025-11-09 11:59:10 -0500

    Multithread day2 for minor gains b/c why not

--- ec25/bin/day2.ml
+++ ec25/bin/day2.ml
@@ -1,3 +1,5 @@
+module C = Domainslib.Chan
+
 let add_complex (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
 
 let mult_complex (x1, y1) (x2, y2) =
@@ -16,7 +18,24 @@ let part1 a =
     (fun acc _ -> add_complex (div_complex (mult_complex acc acc) (10, 10)) a)
     (0, 0) [ 0; 0; 0 ]
 
-let part2_3 scale resolution a =
+let num_domains = 8
+
+type 'a message = Task of 'a | Quit
+
+let c = C.make_bounded (num_domains * 64)
+
+let rec worker f commit state () =
+  match C.recv c with
+  | Task a -> (worker [@tailcall]) f commit (f state a) ()
+  | Quit -> commit state ()
+
+let create_work s =
+  Seq.iter (fun t -> C.send c (Task t)) s;
+  for _ = 1 to num_domains do
+    C.send c Quit
+  done
+
+let count_mandlebrot scale resolution a =
   let n_cells = (scale / resolution) + 1 in
   let check_point pt =
     let _, _, early_exit =
@@ -37,20 +56,37 @@ let part2_3 scale resolution a =
     in
     early_exit
   in
-  Seq.product (Seq.ints 0 |> Seq.take n_cells) (Seq.ints 0 |> Seq.take n_cells)
-  |> Seq.fold_left
-       (fun acc (off_x, off_y) ->
-         let offset = (off_x * resolution, off_y * resolution) in
-         if check_point (add_complex offset a) then acc else acc + 1)
-       0
 
-let part2 = part2_3 1000 10
-let part3 = part2_3 1000 1
+  let count = Atomic.make 0 in
+  let task acc pt = if not (check_point pt) then acc + 1 else acc in
+  let commit_worker_state state () =
+    let _ = Atomic.fetch_and_add count state in
+    ()
+  in
+  (*
+  Unlike the Domain examples, make sure the workers are ready for when we
+  start queuing jobs because otherwise we're not gonna process anything until
+  they're all queued... And we're about to queue a __lot__ of jobs
+  *)
+  let domains =
+    Array.init (num_domains - 1) (fun _ ->
+        Domain.spawn (worker task commit_worker_state 0))
+  in
+  Seq.product (Seq.ints 0 |> Seq.take n_cells) (Seq.ints 0 |> Seq.take n_cells)
+  |> Seq.map (fun (off_x, off_y) ->
+      add_complex a (off_x * resolution, off_y * resolution))
+  |> create_work;
+  worker task commit_worker_state 0 ();
+  Array.iter Domain.join domains;
+  Atomic.get count
+
+let part2 = count_mandlebrot 1000 10
+let part3 = count_mandlebrot 1000 1
 
 let run () =
   let num = Util.read_lines "res/day2.1" |> parse in
   Printf.printf "Part 1: %s\n" (part1 num |> string_of_complex);
   let num = Util.read_lines "res/day2.2" |> parse in
-  Printf.printf "Part 2: %d\n" (part2 num);
+  Printf.printf "Part 2: %s\n" (string_of_int (part2 num));
   let num = Util.read_lines "res/day2.3" |> parse in
-  Printf.printf "Part 3: %d\n" (part3 num)
+  Printf.printf "Part 3: %s\n" (string_of_int (part3 num))
--- ec25/bin/main.ml
+++ ec25/bin/main.ml
@@ -1,1 +1,7 @@
-let () = List.iteri (fun i f -> Printf.printf "### DAY %d ###\n" (i + 1); f ()) [ Day1.run ; Day2.run]
+let () =
+  List.iteri
+    (fun i f ->
+      Printf.printf "### DAY %d ###\n" (i + 1);
+      f ();
+      Printf.printf "%!")
+    [ Day1.run; Day2.run ]
